7dc04e87d82399e5ee38ddb2dd71a2f377a93708
Mariusz Piątkowski <mpxx24@gmail.com>
2016-09-12 21:01:56 +0200

    new fonts,  defined design, basic views

--- MPCV/Controllers/HomeController.cs
+++ MPCV/Controllers/HomeController.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace MPCV.Controllers
-{
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            ViewBag.Title = "Home Page";
+namespace MPCV.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
 
+        public ActionResult PersonalInfo() {
+            return View();
+        }
+
+        public ActionResult ProgrammingSkills() {
+            return View();
+        }
+
+        public ActionResult ProjectExamples() {
+            return View();
+        }
+
+        public ActionResult GithubRepository() {
+            return View();
+        }
+
+        public ActionResult DownloadCv() {
+            return View();
+        }
+
+        public ActionResult Contact() {
             return View();
         }
     }
