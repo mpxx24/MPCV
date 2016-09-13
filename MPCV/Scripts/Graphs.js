967970d511e113ea3c996fc3882d7759e35554fc
Mariusz Piątkowski <mpxx24@gmail.com>
2016-09-13 21:07:20 +0200

    added ucharts.js and a prototype graph on programming skills view

---
+++ MPCV/Scripts/Graphs.js
@@ -0,0 +1,40 @@
+﻿var GraphHelper = function () {
+};
+
+GraphHelper.programmingSkills = function () {
+    var graphdef = {
+        categories: ["Programming skills"],
+        dataset: {
+            "Programming skills": [
+                { name: "C#", value: 75 },
+                { name: "Javascript", value: 55 },
+                { name: "HTML", value: 50 },
+                { name: "CSS", value: 45 }
+            ]
+        }
+    };
+    var chart = uv.chart("Bar", graphdef, {
+            graph: {
+                orientation: "Vertical"
+            },
+            effects: {
+                hovercolor: "#2B4970",
+                strokecolor: "none",
+                textcolor: "#000000",
+                duration: 600,
+                hover: 200,
+                showhovertext: false
+            },
+            dimension: {
+                width: 700,
+                height: 300
+            },
+            axis: {
+                opacity: 0
+            },
+            tooltip: {
+                show: false
+            }
+        }
+    );
+};
