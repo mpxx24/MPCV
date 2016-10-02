cbf280f2a3e829889e2d86ce33f9a58cb66b6ec0
Mariusz Piątkowski <mpxx24@gmail.com>
2016-10-02 12:03:12 +0200

    project fixes, fixed navigation, new test font, basic layout

---
+++ MPCV/Scripts/jquery/animations.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() {
+    var menuLi = $("#side-panel-ul > a");
+
+    $(menuLi).hover(
+        function() {
+            $(this).animate({ backgroundColor: "#00c6ff" }, 50);
+        },
+        function() {
+            $(this).animate({ backgroundColor: "#202020" }, 50);
+        }
+    );
+});
